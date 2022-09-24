d4f9c2f7baab961150740294f387428ffbb01ada
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-09-24 16:41:32 +0000

    parte 1 completada

---
+++ main.py
@@ -0,0 +1,28 @@
+print("\n")
+
+#ejemplo de entero
+x=69
+print(x)
+print(type(x))
+print("\n")
+
+#ejemplo de dato flotante
+e=2.71828182846
+print(e)
+print(type(e)) 
+print("\n")
+#ejemplo de string
+z= "JAJAJAJA que divertida es la programación \n"
+print(z)
+
+#ejemplo de dato booleano
+one= True
+two= False
+print(one)
+print(type(one))
+
+
+
+
+
+
