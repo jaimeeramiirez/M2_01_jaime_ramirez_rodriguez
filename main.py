32af2af2f9678a3a405811bfbb87443117b8bf66
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-09-24 17:05:25 +0000

    he comentado el programa

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ print(one)
 print(type(one))
 print("\n\n")
 
+#Creamos dos variables diferentes para el ejercicio, en el tercer apartado simplemente utilizamos lo aprendido en aritmética báscia y multiplicamos la variable nombre tres veces
 var_nombre= " Jaime"
 apellido= " Ramírez"
 print(var_nombre + apellido)
@@ -29,6 +30,7 @@ print(var_nombre+ apellido + ".")
 print(var_nombre*3)
 print("\n")
 
+#El slicing funciona de tal manera que solo imprimirá por pantalla la franja de caracteres que yo le indique; de esas manera, el programa imprime por pantalla los caracteres del 5 al 13 que coincide con mi apellido
 nombreCompleto= "JAIME RAMÍREZ"
 print (nombreCompleto [5:13])
 
